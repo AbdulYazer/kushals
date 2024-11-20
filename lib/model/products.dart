3777d35ac4857318a8ed488c7834069596d25171
Abdul Yazer N <91891923+AbdulYazer@users.noreply.github.com>
2024-11-20 21:27:17 +0530

    Updated products model

--- lib/model/products.dart
+++ lib/model/products.dart
@@ -1,29 +1,20 @@
 class JewelType {
-  // List<ProductElement> products;
   String jewelType;
   String jewelImageUrl;
-  // int limit;
 
   JewelType({
-    // required this.JewelTypes,
     required this.jewelType,
     required this.jewelImageUrl,
-    // required this.limit,
   });
 
   factory JewelType.fromJson(Map<String, dynamic> json) => JewelType(
-        // products: List<ProductElement>.from(
-        //     json["products"].map((x) => ProductElement.fromJson(x))),
         jewelType: json["jewelType"],
         jewelImageUrl: json["jewelImageUrl"],
-        // limit: json["limit"],
       );
 
   Map<String, dynamic> toJson() => {
-        // "products": List<dynamic>.from(products.map((x) => x.toJson())),
         "jewelType": jewelType,
         "jewelImageUrl": jewelImageUrl,
-        // "limit": limit,
       };
 }
 
